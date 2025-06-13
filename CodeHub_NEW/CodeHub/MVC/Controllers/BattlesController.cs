92be250e3e305e2f92bffabd793b924d27e245e9
Ivan21T <119493817+Ivan21T@users.noreply.github.com>
2025-06-13 12:13:38 +0300

    commit
    new pages

---
+++ CodeHub_NEW/CodeHub/MVC/Controllers/BattlesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodeHub.Controllers
+{
+    public class BattlesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
